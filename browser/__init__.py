fecc1880c66547dbc9e5a5e85c62b290cfaaa5c6
Philipp von Weitershausen <philipp@weitershausen.de>
2004-03-17 17:35:46 +0000

    Import all browser widgets to this central place such that import paths can be shortened to::
    from zope.app.form.browser import FooWidget

--- browser/__init__.py
+++ browser/__init__.py
@@ -1,2 +1,41 @@
+##############################################################################
 #
-# This file is necessary to make this directory a package.
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Browser widgets
+
+$Id: __init__.py,v 1.2 2004/03/17 17:35:46 philikon Exp $
+"""
+from zope.app.form.browser.widget import BrowserWidget, DisplayWidget
+
+from zope.app.form.browser.textwidgets import TextWidget, BytesWidget
+from zope.app.form.browser.textwidgets import TextAreaWidget, BytesAreaWidget
+from zope.app.form.browser.textwidgets import PasswordWidget, FileWidget
+from zope.app.form.browser.textwidgets import ASCIIWidget
+from zope.app.form.browser.textwidgets import IntWidget, FloatWidget
+from zope.app.form.browser.textwidgets import DatetimeWidget, DateWidget
+
+from zope.app.form.browser.enumerated import EnumeratedTextWidget
+from zope.app.form.browser.enumerated import EnumeratedIntWidget
+from zope.app.form.browser.enumerated import EnumeratedFloatWidget
+from zope.app.form.browser.enumerated import EnumeratedDatetimeWidget
+from zope.app.form.browser.enumerated import EnumeratedDateWidget
+
+from zope.app.form.browser.itemswidgets import CheckBoxWidget
+from zope.app.form.browser.itemswidgets import ListWidget, RadioWidget
+from zope.app.form.browser.itemswidgets import MultiListWidget, MultiCheckBoxWidget
+
+from zope.app.form.browser.sequencewidget import SequenceWidget
+from zope.app.form.browser.sequencewidget import TupleSequenceWidget
+from zope.app.form.browser.sequencewidget import ListSequenceWidget
+
+from zope.app.form.browser.objectwidget import ObjectWidget
