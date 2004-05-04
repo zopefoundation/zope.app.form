b566139a02fcc5f27e74ca3cc1a66f3345bfeb60
Garrett Smith <nodelivery@nodelivery.com>
2004-05-04 02:01:25 +0000

    Replaced widget implementation in various forms (edit, display, etc.) with the use of a macro per
      http://dev.zope.org/Zope3/CleanupOfSchemaAndWidgets

---
+++ browser/macros.py
@@ -0,0 +1,23 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""API Documentation macros
+
+$Id: macros.py,v 1.1 2004/05/04 02:01:24 garrett Exp $
+"""
+from zope.app.basicskin.standardmacros import StandardMacros
+
+
+class FormMacros(StandardMacros):
+    
+    macro_pages = ('widget_macros',)
