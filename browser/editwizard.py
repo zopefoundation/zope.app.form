821043b6e6267e03ffab34e29273e5e93292ab3c
Sidnei da Silva <sidnei.da.silva@gmail.com>
2004-07-02 15:10:17 +0000

    Unbroke just enough of the wizards so that it is not completely broken. Widgets have a _widget prefix. 'usage' is gone. Default use_session to false as sessions don't exist yet.

---
+++ browser/editwizard.py
@@ -0,0 +1,238 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Edit Wizard View Classes
+
+$Id$
+"""
+
+from zope.component import getAdapter
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.security.checker import defineChecker, NamesChecker
+
+from zope.app import zapi
+from zope.event import notify
+from zope.app.event.objectevent import ObjectModifiedEvent
+from zope.app.i18n import ZopeMessageIDFactory as _
+from zope.app.location.interfaces import ILocation
+from zope.app.location import LocationProxy
+
+
+from zope.app.pagetemplate.simpleviewclass import SimpleViewClass
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.app.publisher.browser.globalbrowsermenuservice import \
+     globalBrowserMenuService
+
+from editview import EditView
+from submit import Next, Previous, Update
+from zope.app.form.interfaces import WidgetInputError, WidgetsError
+from zope.app.form.utility \
+        import setUpEditWidgets, getWidgetsData, applyWidgetsChanges
+
+
+PaneNumber = 'CURRENT_PANE_IDX'
+
+# TODO: Needs to be persistent aware for session (?)
+class WizardStorage(dict):
+    def __init__(self, fields, content):
+        super(WizardStorage, self).__init__(self)
+        if content:
+            for k in fields:
+                self[k] = getattr(content,k)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError, key
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+class EditWizardView(EditView):
+
+    def _setUpWidgets(self):
+        adapted = getAdapter(self.context, self.schema)
+        if adapted is not self.context:
+            if not ILocation.providedBy(adapted):
+                adapted = LocationProxy(adapted)
+            adapted.__parent__ = self.context
+        self.adapted = adapted
+
+        if self.use_session:
+            # Need session for File upload fields
+            raise NotImplementedError, \
+                'Cannot be implemented until we have an ISessionDataManager'
+        else:
+            self.storage = WizardStorage(self.fieldNames, adapted)
+
+        # Add all our widgets as attributes on this view
+        setUpEditWidgets(self, self.schema, source=self.storage,
+                         names=self.fieldNames)
+
+    def widgets(self):
+        return [getattr(self, name+'_widget')
+            for name in self.currentPane().names
+            ]
+
+    _current_pane_idx = 0
+
+    def currentPane(self):
+        return self.panes[self._current_pane_idx]
+
+    _update_called = 0
+
+    # Message rendered at the top of the form, probably set by update()
+    feedback = u''
+
+    def update(self):
+        '''Called before rendering each pane. It is responsible
+        for extracting data into temporary storage, and selecting
+        which pane should be rendered.
+        '''
+        # Calling twice does nothing
+        if self._update_called:
+            return
+        self._update_called = 1
+
+        # Determine the current pane
+        if PaneNumber in self.request:
+            self._current_pane_idx = int(self.request[PaneNumber])
+            assert self._current_pane_idx >= 0
+            assert self._current_pane_idx < len(self.panes)
+        else:
+            # First page
+            self._current_pane_idx = 0
+            self.errors = {}
+            self.label = self.currentPane().label
+            self._choose_buttons()
+            return
+
+        # Validate the current pane, and set self.errors
+        try:
+            names = self.currentPane().names
+            data = getWidgetsData(self, self.schema, names=names)
+            self.errors = {}
+        except WidgetsError, errors:
+            x = {}
+            for k, label, msg in errors:
+                x[k] = msg
+            self.errors = x
+        else:
+
+            self.storage.update(data)
+
+            if Next in self.request:
+                self._current_pane_idx += 1
+                assert self._current_pane_idx < len(self.panes)
+            elif Previous in self.request:
+                self._current_pane_idx -= 1
+                assert self._current_pane_idx >= 0
+            elif Update in self.request:
+                if not self.use_session:
+                    # Data from panes other than the current one is still
+                    # stuck in request
+                    self.storage.update(getWidgetsData(
+                        self, self.schema, names=self.fieldNames))
+                if self.apply_update(self.storage):
+                    self.feedback = _(u'No changes to save')
+                else:
+                    self.feedback = _(u'Changes saved')
+
+        # Set the current label
+        self.label = self.currentPane().label
+
+        self._choose_buttons()
+
+    def _choose_buttons(self):
+        '''Determine what buttons appear when we render the current pane'''
+
+        # The submit button appears if every field on every pane except the
+        # current one has valid input or a valid default value.
+        # This is almost always the case for edit forms.
+        try:
+            for k in self.fieldNames:
+                if k not in self.currentPane().names:
+                    getattr(self, k+'_widget').getInputValue()
+            self.show_submit = 1
+        except WidgetInputError:
+            self.show_submit = 0
+
+        self.show_next = (self._current_pane_idx < len(self.panes) - 1)
+
+        self.show_previous = self._current_pane_idx > 0
+
+    def apply_update(self, storage):
+        ''' Save changes to our content object '''
+        for k,v in storage.items():
+            getattr(self,k+'_widget').setRenderedValue(v)
+        content = self.adapted
+        changed = applyWidgetsChanges(self, self.schema, target=content,
+                names=self.fieldNames)
+        # We should not generate events when an adapter is used.
+        # That's the adapter's job
+        if changed and self.context is self.adapted:
+            notify(ObjectModifiedEvent(content))
+        return not changed
+
+    def renderHidden(self):
+        ''' Render state as hidden fields. Also render hidden fields to
+            propagate self.storage if we are not using the session to do this.
+        '''
+        olist = []
+        out = olist.append
+
+        # the index of the pane being rendered needs to be propagated
+        out('<input class="hiddenType" type="hidden" name="%s" value="%d" />'%(
+            PaneNumber, self._current_pane_idx
+            ))
+
+        if self.use_session:
+            # Need to output a unique key as a hidden field to identity this
+            # particular wizard. We use this to ensure data for this view
+            # doesn't conflict with other wizards in progress in other
+            # browser windows.
+            # Otherwise, no more state to propagate
+            raise NotImplementedError, 'use_session'
+
+        else:
+            current_fields = self.currentPane().names
+            for k in self.fieldNames:
+                if k not in current_fields:
+                    widget = getattr(self, k+'_widget')
+                    out(widget.hidden())
+            return ''.join(olist)
+
+
+def EditWizardViewFactory(name, schema, permission, layer,
+                    panes, fields, template, default_template, bases, for_,
+                    menu=u'', use_session=False):
+    # XXX What about the __implements__ of the bases?
+    class_ = SimpleViewClass(template, used_for=schema, bases=bases)
+    class_.schema = schema
+    class_.panes = panes
+    class_.fieldNames = fields
+    class_.use_session = use_session
+
+    class_.generated_form = ViewPageTemplateFile(default_template)
+
+    defineChecker(
+        class_,
+        NamesChecker(("__call__", "__getitem__", "browserDefault"), permission)
+        )
+
+    s = zapi.getGlobalService(zapi.servicenames.Presentation)
+    s.provideView(for_, name, IBrowserRequest, class_, layer)
+
+
---
+++ browser/metaconfigure.py
@@ -0,0 +1,329 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Configuration handlers for forms and widgets
+
+$Id$
+"""
+import os
+
+from zope.interface import implementedBy
+from zope.configuration.exceptions import ConfigurationError
+
+from zope.schema import getFieldNamesInOrder
+from zope.app.container.interfaces import IAdding
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.app.publisher.browser.globalbrowsermenuservice import \
+     menuItemDirective
+
+from zope.app.form import CustomWidgetFactory
+from add import AddView, AddViewFactory
+from editview import EditView, EditViewFactory
+from addwizard import AddWizardView, AddWizardViewFactory
+from editwizard import EditWizardView, EditWizardViewFactory
+from schemadisplay import DisplayView, DisplayViewFactory
+
+class BaseFormDirective(object):
+
+    # to be overriden by the subclasses
+    view = None
+    default_template = None
+
+    # default basic information
+    for_ = None
+    layer = 'default'
+    permission = 'zope.Public'
+    template = None
+    class_ = None
+
+    # default form information
+    title = None
+    label = None
+    menu = None
+    fields = None
+
+    def __init__(self, _context, **kwargs):
+        self._context = _context
+        for key, value in kwargs.items():
+            if not (value is None and hasattr(self, key)):
+                setattr(self, key, value)
+        self._normalize()
+        self._widgets = {}
+
+    def widget(self, _context, field, class_, **kw):
+        attrs = kw
+        ifaces = implementedBy(class_)
+        # Try to do better than accepting the string value by looking through
+        # the interfaces and trying to find the field, so that we can use
+        # 'fromUnicode()' 
+        for name, value in kw.items():
+            for iface in ifaces:
+                if name in iface:
+                    attrs[name] = iface[name].fromUnicode(value)
+                    break
+
+        self._widgets[field+'_widget'] = CustomWidgetFactory(class_, **attrs) 
+
+    def _processWidgets(self):
+        if self._widgets:
+            customWidgetsObject = type('CustomWidgetsMixin', (object,),
+                                       self._widgets) 
+            self.bases = self.bases + (customWidgetsObject,)
+
+    def _normalize(self):
+        if self.for_ is None:
+            self.for_ = self.schema
+
+        if self.class_ is None:
+            self.bases = (self.view,)
+        else:
+            self.bases = (self.class_, self.view)
+
+        if self.template is not None:
+            self.template = os.path.abspath(str(self.template))
+            if not os.path.isfile(self.template):
+                raise ConfigurationError("No such file", self.template)
+        else:
+            self.template = self.default_template
+
+        self.names = getFieldNamesInOrder(self.schema)
+
+        if self.fields:
+            for name in self.fields:
+                if name not in self.names:
+                    raise ValueError("Field name is not in schema",
+                                     name, self.schema)
+        else:
+            self.fields = self.names
+
+    def _args(self):
+        return (self.name, self.schema, self.label, self.permission,
+                self.layer, self.template, self.default_template,
+                self.bases, self.for_, self.fields)
+
+    def _discriminator(self):
+        return ('view', self.for_, self.name, IBrowserRequest,
+                self.layer)
+
+
+class Pane(object):
+    ''' Holder for information about a Pane of a wizard '''
+    # TODO: Add more funky stuff to each pane, such as a validator
+    def __init__(self, field_names, label):
+        self.names = field_names
+        self.label = label
+
+
+class BaseWizardDirective(BaseFormDirective):
+
+    # default wizard information
+    description = None
+    use_session = False
+
+    def __init__(self, _context, **kwargs):
+        super(BaseWizardDirective, self).__init__(_context, **kwargs)
+        self.panes = []
+
+    def _args(self):
+        return (self.name, self.schema, self.permission, self.layer,
+                self.panes, self.fields, self.template, self.default_template,
+                self.bases, self.for_)
+
+    def pane(self, _context, fields, label=''):
+        for f in fields:
+            if f not in self.fields:
+                raise ValueError(
+                    'Field name is not in schema',
+                    f, self.schema
+                    )
+        self.panes.append(Pane(fields, label))
+
+class AddFormDirective(BaseFormDirective):
+
+    view = AddView
+    default_template = 'add.pt'
+    for_ = IAdding
+
+    # default add form information
+    description = None
+    content_factory = None
+    arguments = None
+    keyword_arguments = None
+    set_before_add = None
+    set_after_add = None
+
+    def _handle_menu(self):
+        if self.menu or self.title:
+            if (not self.menu) or (not self.title):
+                raise ValueError("If either menu or title are specified, "
+                                 "they must both be specified")
+            # XXX why no self.schema in for as in EditFormDirective
+            menuItemDirective(
+                self._context, self.menu, self.for_, '@@' + self.name,
+                self.title, permission=self.permission,
+                description=self.description)
+
+    def _handle_arguments(self, leftover=None):
+        schema = self.schema
+        fields = self.fields
+        arguments = self.arguments
+        keyword_arguments = self.keyword_arguments
+        set_before_add = self.set_before_add
+        set_after_add = self.set_after_add
+
+        if leftover is None:
+            leftover = fields
+
+        if arguments:
+            missing = [n for n in arguments if n not in fields]
+            if missing:
+                raise ValueError("Some arguments are not included in the form",
+                                 missing)
+            optional = [n for n in arguments if not schema[n].required]
+            if optional:
+                raise ValueError("Some arguments are optional, use"
+                                 " keyword_arguments for them",
+                                 optional)
+            leftover = [n for n in leftover if n not in arguments]
+
+        if keyword_arguments:
+            missing = [n for n in keyword_arguments if n not in fields]
+            if missing:
+                raise ValueError(
+                    "Some keyword_arguments are not included in the form",
+                    missing)
+            leftover = [n for n in leftover if n not in keyword_arguments]
+
+        if set_before_add:
+            missing = [n for n in set_before_add if n not in fields]
+            if missing:
+                raise ValueError(
+                    "Some set_before_add are not included in the form",
+                    missing)
+            leftover = [n for n in leftover if n not in set_before_add]
+
+        if set_after_add:
+            missing = [n for n in set_after_add if n not in fields]
+            if missing:
+                raise ValueError(
+                    "Some set_after_add are not included in the form",
+                    missing)
+            leftover = [n for n in leftover if n not in set_after_add]
+
+            self.set_after_add += leftover
+
+        else:
+            self.set_after_add = leftover
+
+    def __call__(self):
+        self._processWidgets()
+        self._handle_menu()
+        self._handle_arguments()
+
+        self._context.action(
+            discriminator=self._discriminator(),
+            callable=AddViewFactory,
+            args=self._args()+(self.content_factory, self.arguments,
+                                 self.keyword_arguments,
+                                 self.set_before_add, self.set_after_add),
+            kw={'menu': self.menu},
+            )
+
+class EditFormDirective(BaseFormDirective):
+
+    view = EditView
+    default_template = 'edit.pt'
+    title = 'Edit'
+
+    def _handle_menu(self):
+        if self.menu:
+            menuItemDirective(
+                self._context, self.menu, self.for_ or self.schema,
+                '@@' + self.name, self.title, permission=self.permission)
+
+    def __call__(self):
+        self._processWidgets()
+        self._handle_menu()
+        self._context.action(
+            discriminator=self._discriminator(),
+            callable=EditViewFactory,
+            args=self._args(),
+            kw={'menu': self.menu},
+        )
+
+class SubeditFormDirective(BaseFormDirective):
+
+    view = EditView
+    default_template = 'subedit.pt'
+
+    # default subedit form directive
+    fulledit_path = None
+    fulledit_label = None
+
+    def __call__(self):
+        self._processWidgets()
+        self._context.action(
+            discriminator = self._discriminator(),
+            callable = EditViewFactory,
+            args = self._args()+(self.fulledit_path, self.fulledit_label),
+            )
+
+class AddWizardDirective(BaseWizardDirective, AddFormDirective):
+
+    view = AddWizardView
+    default_template = 'addwizard.pt'
+
+    def __call__(self):
+        self._handle_menu()
+
+        all_fields = self.fields
+        leftover = []
+        for pane in self.panes:
+            leftover.extend(pane.names)
+        self.fields = leftover[:]
+        self._handle_arguments(leftover)
+        self.fields = all_fields
+
+        self._context.action(
+            discriminator = self._discriminator(),
+            callable = AddWizardViewFactory,
+            args = self._args()+(self.content_factory, self.arguments,
+                                 self.keyword_arguments, self.set_before_add,
+                                 self.set_after_add, self.use_session)
+            )
+
+class EditWizardDirective(BaseWizardDirective, EditFormDirective):
+
+    view = EditWizardView
+    default_template = 'editwizard.pt'
+
+    def __call__(self):
+        self._handle_menu()
+        self._context.action(
+            discriminator = self._discriminator(),
+            callable = EditWizardViewFactory,
+            args = self._args()+(self.menu, self.use_session)
+            )
+
+class SchemaDisplayDirective(EditFormDirective):
+
+    view = DisplayView
+    default_template = 'display.pt'
+
+    def __call__(self):
+        self._handle_menu()
+        self._context.action(
+            discriminator = self._discriminator(),
+            callable = DisplayViewFactory,
+            args = self._args()+(self.menu,)
+            )
