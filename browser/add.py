0b85ec45246e1a7cb007c07bf885dc37b6aba48e
Jim Fulton <jim@zope.com>
2004-07-06 18:48:48 +0000

    Merged from trunk r26049:
    Updated adapter API
    
    as described in:
    
      http://mail.zope.org/pipermail/zope3-dev/2004-July/011537.html
    
    - query/getAdapter now require a name argument and ignore
      whether the object being adapted provides or conforms to the
      interface.
    
    - New functions 'getAdapterInContext' and 'queryAdapterInContext'
      can be used to do content-dependent simple adapation.  They
      will return the object being adapted if it provides the desired
      interface and will return the result of calling __conform__ if the
      object conforms to the interface.
    
    IMPORTANT
    
      To do simple adaptation, just call the desired interface:
    
        adapter = iface(ob) or iface(ob, default)

---
+++ browser/add.py
@@ -0,0 +1,163 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Add Form View class
+
+$Id$
+"""
+import sys
+
+from zope.app import zapi
+from zope.event import notify
+from zope.app.event.objectevent import ObjectCreatedEvent
+from zope.app.form.utility import setUpWidgets, getWidgetsData
+from zope.app.i18n import ZopeMessageIDFactory as _
+from zope.app.form.interfaces import IInputWidget, WidgetsError
+from zope.app.pagetemplate.simpleviewclass import SimpleViewClass
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.schema.interfaces import ValidationError
+from zope.security.checker import defineChecker, NamesChecker
+from zope.app.publisher.browser.globalbrowsermenuservice import \
+    globalBrowserMenuService
+from editview import EditView
+from submit import Update
+
+class AddView(EditView):
+    """Simple edit-view base class.
+
+    Subclasses should provide a schema attribute defining the schema
+    to be edited.
+    """
+
+    def _setUpWidgets(self):
+        setUpWidgets(self, self.schema, IInputWidget, names=self.fieldNames)
+
+    def update(self):
+
+        if self.update_status is not None:
+            # We've been called before. Just return the previous result.
+            return self.update_status
+
+        if Update in self.request:
+
+            self.update_status = ''
+            try:
+                data = getWidgetsData(self, self.schema, names=self.fieldNames)
+                self.createAndAdd(data)
+            except WidgetsError, errors:
+                self.errors = errors
+                self.update_status = _("An error occured.")
+                return self.update_status
+
+            self.request.response.redirect(self.nextURL())
+
+        return self.update_status
+
+    def create(self, *args, **kw):
+        """Do the actual instantiation."""
+        return self._factory(*args, **kw)
+
+    def createAndAdd(self, data):
+        """Add the desired object using the data in the data argument.
+
+        The data argument is a dictionary with the data entered in the form.
+        """
+
+        args = []
+        if self._arguments:
+            for name in self._arguments:
+                args.append(data[name])
+
+        kw = {}
+        if self._keyword_arguments:
+            for name in self._keyword_arguments:
+                if name in data:
+                    kw[str(name)] = data[name]
+
+        content = self.create(*args, **kw)
+        adapted = self.schema(content)
+
+        errors = []
+
+        if self._set_before_add:
+            for name in self._set_before_add:
+                if name in data:
+                    field = self.schema[name]
+                    try:
+                        field.set(adapted, data[name])
+                    except ValidationError:
+                        errors.append(sys.exc_info()[1])
+
+        if errors:
+            raise WidgetsError(*errors)
+
+        notify(ObjectCreatedEvent(content))
+
+        content = self.add(content)
+
+        adapted = self.schema(content)
+
+        if self._set_after_add:
+            for name in self._set_after_add:
+                if name in data:
+                    field = self.schema[name]
+                    try:
+                        field.set(adapted, data[name])
+                    except ValidationError:
+                        errors.append(sys.exc_info()[1])
+
+        if errors:
+            raise WidgetsError(*errors)
+
+        return content
+
+    def add(self, content):
+        return self.context.add(content)
+
+    def nextURL(self):
+        return self.context.nextURL()
+
+
+def AddViewFactory(name, schema, label, permission, layer,
+                   template, default_template, bases, for_,
+                   fields, content_factory, arguments,
+                   keyword_arguments, set_before_add, set_after_add,
+                   menu=u''):
+
+    s = zapi.getGlobalService(zapi.servicenames.Presentation)
+    class_  = SimpleViewClass(
+        template,
+        used_for = schema, bases = bases
+        )
+
+    class_.schema = schema
+    class_.label = label
+    class_.fieldNames = fields
+    class_._factory = content_factory
+    class_._arguments = arguments
+    class_._keyword_arguments = keyword_arguments
+    class_._set_before_add = set_before_add
+    class_._set_after_add = set_after_add
+
+    class_.generated_form = ViewPageTemplateFile(default_template)
+
+    defineChecker(class_,
+                  NamesChecker(
+                    ("__call__", "__getitem__",
+                     "browserDefault", "publishTraverse"),
+                    permission,
+                    )
+                  )
+
+    s.provideView(for_, name, IBrowserRequest, class_, layer)
---
+++ browser/schemadisplay.py
@@ -0,0 +1,90 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Support for display-only pages based on schema.
+
+$Id$
+"""
+from zope.app import zapi
+
+from zope.schema import getFieldNamesInOrder
+
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.app.location.interfaces import ILocation
+from zope.app.location import LocationProxy
+from zope.app.publisher.browser import BrowserView
+from zope.security.checker import defineChecker, NamesChecker
+
+from zope.app.form.utility import setUpDisplayWidgets
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.app.pagetemplate.simpleviewclass import SimpleViewClass
+
+class DisplayView(BrowserView):
+    """Simple display-view base class.
+
+    Subclasses should provide a schema attribute defining the schema
+    to be displayed.
+    """
+
+    errors = ()
+    update_status = ''
+    label = ''
+
+    # Fall-back field names computes from schema
+    fieldNames = property(lambda self: getFieldNamesInOrder(self.schema))
+
+    def __init__(self, context, request):
+        super(DisplayView, self).__init__(context, request)
+        self._setUpWidgets()
+
+    def _setUpWidgets(self):
+        adapted = self.schema(self.context)
+        if adapted is not self.context:
+            if not ILocation.providedBy(adapted):
+                adapted = LocationProxy(adapted)
+            adapted.__parent__ = self.context
+        self.adapted = adapted
+        setUpDisplayWidgets(self, self.schema, source=adapted,
+                            names=self.fieldNames)
+
+    def setPrefix(self, prefix):
+        for widget in self.widgets():
+            widget.setPrefix(prefix)
+
+    def widgets(self):
+        return [getattr(self, name+'_widget')
+                for name in self.fieldNames]
+
+
+def DisplayViewFactory(name, schema, label, permission, layer,
+                       template, default_template, bases, for_, fields,
+                       fulledit_path=None, fulledit_label=None, menu=u'',
+                       usage=u''):
+    # XXX What about the __implements__ of the bases?
+    class_ = SimpleViewClass(template, used_for=schema, bases=bases)
+    class_.schema = schema
+    class_.label = label
+    class_.fieldNames = fields
+    class_.fulledit_path = fulledit_path
+    if fulledit_path and (fulledit_label is None):
+        fulledit_label = "Full display"
+    class_.fulledit_label = fulledit_label
+    class_.generated_form = ViewPageTemplateFile(default_template)
+    class_.usage = usage or (
+        menu and globalBrowserMenuService.getMenuUsage(menu)
+        )
+    defineChecker(class_,
+                  NamesChecker(("__call__", "__getitem__", "browserDefault"),
+                               permission))
+    s = zapi.getGlobalService(zapi.servicenames.Presentation)
+    s.provideView(for_, name, IBrowserRequest, class_, layer)
