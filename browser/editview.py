a9114988a20108bfe0590bccb8948963b7c0d87f
Tim Peters <tim.one@comcast.net>
2005-03-31 15:50:20 +0000

    Stop using the deprecated get_transaction().
    Alas, there's not enough stuff in this branch to run tests,
    so none of this has been tested yet.

---
+++ browser/editview.py
@@ -0,0 +1,146 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Edit View Classes
+
+$Id$
+"""
+from datetime import datetime
+import transaction
+
+from zope.schema import getFieldNamesInOrder
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.security.checker import defineChecker, NamesChecker
+
+from zope.app import zapi
+from zope.event import notify
+from zope.app.event.objectevent import ObjectModifiedEvent
+from zope.app.i18n import ZopeMessageIDFactory as _
+from zope.app.form.interfaces import WidgetsError
+from zope.app.location.interfaces import ILocation
+from zope.app.location import LocationProxy
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.app.pagetemplate.simpleviewclass import SimpleViewClass
+from zope.app.publisher.browser import BrowserView
+from zope.app.publisher.browser.globalbrowsermenuservice import \
+    globalBrowserMenuService
+
+from zope.app.form.utility import setUpEditWidgets, applyWidgetsChanges
+from zope.app.form.browser.submit import Update
+
+
+class EditView(BrowserView):
+    """Simple edit-view base class
+
+    Subclasses should provide a schema attribute defining the schema
+    to be edited.
+    """
+
+    errors = ()
+    update_status = None
+    label = ''
+
+    # Fall-back field names computes from schema
+    fieldNames = property(lambda self: getFieldNamesInOrder(self.schema))
+    # Fall-back template
+    generated_form = ViewPageTemplateFile('edit.pt')
+
+    def __init__(self, context, request):
+        super(EditView, self).__init__(context, request)
+        self._setUpWidgets()
+
+    def _setUpWidgets(self):
+        adapted = self.schema(self.context)
+        if adapted is not self.context:
+            if not ILocation.providedBy(adapted):
+                adapted = LocationProxy(adapted)
+            adapted.__parent__ = self.context
+        self.adapted = adapted
+        setUpEditWidgets(self, self.schema, source=self.adapted, 
+                         names=self.fieldNames)
+
+    def setPrefix(self, prefix):
+        for widget in self.widgets():
+            widget.setPrefix(prefix)
+
+    def widgets(self):
+        return [getattr(self, name+'_widget')
+                for name in self.fieldNames]
+
+    def changed(self):
+        # This method is overridden to execute logic *after* changes
+        # have been made.
+        pass
+
+    def update(self):
+        if self.update_status is not None:
+            # We've been called before. Just return the status we previously
+            # computed.
+            return self.update_status
+
+        status = ''
+
+        content = self.adapted
+
+        if Update in self.request:
+            changed = False
+            try:
+                changed = applyWidgetsChanges(self, self.schema,
+                    target=content, names=self.fieldNames)
+                # We should not generate events when an adapter is used.
+                # That's the adapter's job.
+                if changed and self.context is self.adapted:
+                    notify(ObjectModifiedEvent(content))
+            except WidgetsError, errors:
+                self.errors = errors
+                status = _("An error occured.")
+                transaction.abort()
+            else:
+                setUpEditWidgets(self, self.schema, source=self.adapted,
+                                 ignoreStickyValues=True, 
+                                 names=self.fieldNames)
+                if changed:
+                    self.changed()
+                    formatter = self.request.locale.dates.getFormatter(
+                        'dateTime', 'medium')
+                    status = _("Updated on ${date_time}")
+                    status.mapping = {'date_time': formatter.format(
+                        datetime.utcnow())}
+
+        self.update_status = status
+        return status
+
+
+def EditViewFactory(name, schema, label, permission, layer,
+                    template, default_template, bases, for_, fields,
+                    fulledit_path=None, fulledit_label=None, menu=u''):
+    s = zapi.getGlobalService(zapi.servicenames.Presentation)
+    class_ = SimpleViewClass(template, used_for=schema, bases=bases)
+    class_.schema = schema
+    class_.label = label
+    class_.fieldNames = fields
+
+    class_.fulledit_path = fulledit_path
+    if fulledit_path and (fulledit_label is None):
+        fulledit_label = "Full edit"
+
+    class_.fulledit_label = fulledit_label
+
+    class_.generated_form = ViewPageTemplateFile(default_template)
+
+    defineChecker(class_,
+                  NamesChecker(("__call__", "__getitem__",
+                                "browserDefault", "publishTraverse"),
+                               permission))
+
+    s.provideView(for_, name, IBrowserRequest, class_, layer)
---
+++ browser/ftests/test_filewidget.py
@@ -0,0 +1,176 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""File Widget Tests
+
+$Id$
+"""
+import unittest
+from StringIO import StringIO
+from persistent import Persistent
+import transaction
+
+from zope.interface import Interface
+from zope.interface import implements
+
+from zope.schema.interfaces import IField
+from zope.schema import Field
+
+from zope.app.form.browser.textwidgets import FileWidget
+
+from support import *
+from zope.app.traversing.api import traverse
+
+from zope.app.tests.functional import BrowserTestCase
+from zope.app.form.interfaces import IInputWidget
+
+class IFileField(IField):
+    """Field for representing a file that can be edited by FileWidget."""
+
+
+class FileField(Field):
+
+    implements(IFileField)
+
+
+defineWidgetView(IFileField, FileWidget, IInputWidget)
+
+
+class IFileTest(Interface):
+
+    f1 = FileField(required=True)
+    f2 = FileField(required=False)
+
+registerEditForm(IFileTest)
+
+
+class FileTest(Persistent):
+
+    implements(IFileTest)
+
+    def __init__(self):
+        self.f1 = None
+        self.f2 = 'foo'
+
+defineSecurity(FileTest, IFileTest)
+
+
+class SampleTextFile(StringIO):
+
+    def __init__(self, buf, filename=''):
+        StringIO.__init__(self, buf)
+        self.filename = filename
+
+
+class Test(BrowserTestCase):
+
+    sampleText = "The quick brown fox\njumped over the lazy dog."
+    sampleTextFile = SampleTextFile(sampleText)
+
+    emptyFileName = 'empty.txt'
+    emptyFile = SampleTextFile('', emptyFileName)
+
+
+    def test_display_editform(self):
+        self.getRootFolder()['test'] = FileTest()
+        transaction.commit()
+
+        # display edit view
+        response = self.publish('/test/edit.html')
+        self.assertEqual(response.getStatus(), 200)
+
+        # field should be displayed in a file input element
+        self.assert_(patternExists(
+            '<input .* name="field.f1".* type="file".*>', response.getBody()))
+        self.assert_(patternExists(
+            '<input .* name="field.f2".* type="file".*>', response.getBody()))
+
+
+    def test_submit_text(self):
+        self.getRootFolder()['test'] = FileTest()
+        transaction.commit()
+        object = traverse(self.getRootFolder(), 'test')
+        self.assert_(object.f1 is None)
+        self.assertEqual(object.f2, 'foo')
+
+        # submit a sample text file
+        response = self.publish('/test/edit.html', form={
+            'UPDATE_SUBMIT' : '',
+            'field.f1' : self.sampleTextFile,
+            'field.f2' : self.sampleTextFile })
+        self.assertEqual(response.getStatus(), 200)
+        self.assert_(updatedMsgExists(response.getBody()))
+
+        # check new values in object
+        object = traverse(self.getRootFolder(), 'test')
+        self.assertEqual(object.f1, self.sampleText)
+        self.assertEqual(object.f2, self.sampleText)
+
+
+    def test_invalid_value(self):
+        self.getRootFolder()['test'] = FileTest()
+        transaction.commit()
+
+        # submit an invalid file value
+        response = self.publish('/test/edit.html', form={
+            'UPDATE_SUBMIT' : '',
+            'field.f1' : 'not a file - same as missing input' })
+        self.assertEqual(response.getStatus(), 200)
+        self.assert_(validationErrorExists('f1',
+            'Form input is not a file object', response.getBody()))
+
+
+    # For some reason this test does not work, which means that the missing
+    # input recognition of file widgets does not work correctly. I just lost
+    # my patience looking at it. 
+    def XXX_test_required_validation(self):
+        self.getRootFolder()['test'] = FileTest()
+        transaction.commit()
+
+        # submit missing value for required field f1
+        response = self.publish('/test/edit.html', form={
+            'UPDATE_SUBMIT' : ''})
+        self.assertEqual(response.getStatus(), 200)
+
+        print response.getBody()
+
+        # confirm error msgs
+        self.assert_(missingInputErrorExists('f1', response.getBody()))
+        self.assert_(not missingInputErrorExists('f2', response.getBody()))
+
+
+    def test_empty_file(self):
+        self.getRootFolder()['test'] = FileTest()
+        transaction.commit()
+
+        # submit an empty text file
+        response = self.publish('/test/edit.html', form={
+            'UPDATE_SUBMIT' : '',
+            'field.f2' : self.emptyFile })
+        self.assertEqual(response.getStatus(), 200)
+        self.assert_(updatedMsgExists(response.getBody()))
+
+        # new value for f1 should be field.missing_value (i.e, None)
+        object = traverse(self.getRootFolder(), 'test')
+        self.assert_(object.f1 is None)
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(Test))
+    return suite
+
+if __name__=='__main__':
+    unittest.main(defaultTest='test_suite')
+
+
