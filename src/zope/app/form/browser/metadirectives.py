57443cc5f72b5a0b1615a82a4d2051f25b4381cf
Sidnei da Silva <sidnei.da.silva@gmail.com>
2008-10-13 19:04:00 +0000

    - Remove reference to deprecated LayerField

--- src/zope/app/form/browser/metadirectives.py
+++ src/zope/app/form/browser/metadirectives.py
@@ -23,7 +23,6 @@ from zope.configuration.fields import Tokens, Path, Bool, PythonIdentifier
 from zope.configuration.fields import MessageID
 from zope.schema import Text, TextLine, Id
 from zope.security.zcml import Permission
-from zope.app.component.back35 import LayerField
 from zope.app.publisher.browser.fields import MenuField
 
 class ICommonInformation(Interface):
@@ -63,7 +62,7 @@ class ICommonInformation(Interface):
         required=True
         )
 
-    layer = LayerField(
+    layer = GlobalInterface(
         title=u"Layer",
         description=u"The later the view is in. Default: 'default'",
         required=False
