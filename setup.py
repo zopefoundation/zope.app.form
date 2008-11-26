c3f2c056c2025cbf1eb4b512552640c31dbed3f9
Christian Zagrodnick <cz@gocept.com>
2008-11-26 10:02:36 +0000

    3.6.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.form',
-      version = '3.6.4dev',
+      version = '3.6.4',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='The Original Zope 3 Form Framework',
