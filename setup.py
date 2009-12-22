43b2d7739134c8a9671355a1021c7be2dac1dc9e
Hanno Schlichting <hanno@hannosch.eu>
2009-12-22 21:51:18 +0000

    Prepare zope.app.form 3.12.1.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.form',
-      version = '3.12.1dev',
+      version = '3.12.1',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='The Original Zope 3 Form Framework',
